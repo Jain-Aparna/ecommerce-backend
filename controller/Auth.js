ebdc99a1c1e192af11ebaffa022927cd54899aa0
Aparna Jain <aj.aparnajain13@gmail.com>
2023-08-04 21:41:03 +0530

    backend

---
+++ controller/Auth.js
@@ -0,0 +1,50 @@
+const passport = require("passport");
+const User = require("../model/User");
+const crypto = require("crypto");
+const { sanitizeUser } = require("../services/common");
+const SECRET_KEY = 'SECRET_KEY';
+const jwt = require('jsonwebtoken')
+exports.createUser = async (req, res) => {
+  try {
+    const salt = crypto.randomBytes(16);
+    crypto.pbkdf2(
+      req.body.password,
+      salt,
+      310000,
+      32,
+      "sha256",
+      async function (err, hashedPassword) {
+        const user = new User({ ...req.body, passport: hashedPassword, salt });
+        const doc = await user.save();
+
+        req.login(sanitizeUser(doc), (error) => {
+          if (error) {
+            res.status(400).json(error);
+          } else {
+            const token = jwt.sign(sanitizeUser(doc), process.env.JWT_SECRET_KEY);
+            res.cookie('jwt', token, {expires:new Date(Date.now() + 3600000), httpOnly : true})
+            res.status(201).json({ id: doc.id, role: doc.role });
+          }
+        });
+      }
+    );
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+exports.loginUser = async (req, res) => {
+    const user = req.user;
+    res.cookie('jwt', user.token, {expires:new Date(Date.now() + 3600000), httpOnly : true})
+  .status(201).json({id:user.id, role:user.role});
+};
+
+exports.checkAuth = async (req, res) => {
+    if(req.user)
+    {
+        res.json(req.user);
+    }else{
+        res.sendStatus(401)
+    }
+  
+};
